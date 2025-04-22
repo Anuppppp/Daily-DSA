5d31409298db040e43540d8eaa1908fc20ef4a57
Anup <dev-024@icingtree.com>
2025-04-22 22:53:00 +0530

    Computing power efficent approach

--- DSA-Mathematics/computing-power.py
+++ DSA-Mathematics/computing-power.py
@@ -16,13 +16,28 @@ Input: x = 7, n = 2
 Output: 49   // 7*7
 """
 
-#  Naive solution
+#  Naive Approach
+
+# def computing_power(x, n):
+#     res = 1
+#     for i in range(n):
+#         res = res * x
+#     return res
+
+
+
+
+
+# Efficient Approach
 
 def computing_power(x, n):
-    res = 1
-    for i in range(n):
-        res = res * x
-    return res
-
+    if n == 0:
+        return 1
+    temp = computing_power(x, n//2)
+    temp = temp * temp
+    if n % 2 == 0:
+        return temp
+    else:
+        return temp * x
 
 print(computing_power(2, 3))
